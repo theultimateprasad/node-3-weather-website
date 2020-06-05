3cf177b7dc08239a253938bf7dc2abdcbde0d128
theultimateprasad <prasad.raspaile@gmail.com>
2020-06-05 16:23:25 +0530

    Initi Commit

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,4 @@
-// import { response } from "express"
-
-console.log('Client side javascript file is loaded!')
+// console.log('Client side javascript file is loaded!')
 
 fetch('http://puzzle.mead.io/puzzle').then((response) => {
     response.json().then((data) => {
